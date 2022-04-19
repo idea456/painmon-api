9e0f6b0bef00defad71432be2c269b14abd218bf
Adrienne Rio Wongso Atmojo <aatm0001@student.monash.edu>
2022-04-19 15:39:52 +0800

    removed date checking

--- src/resolvers/dailyResolver.ts
+++ src/resolvers/dailyResolver.ts
@@ -9,6 +9,136 @@ import { CHARACTERS } from "../data/characters";
 import { Weapon } from "../schemas/weapon";
 import { WEAPONS } from "../data/weapons";
 
+export async function generateDailyData() : Promise<Daily> {
+    const today = new Date(Date.now());
+    const items = {};
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const day = days[today.getDay()];
+    let materials: Array<ItemGroup>;
+    // make sure to test this
+    materials = await compare<ItemGroup>(
+        ITEM_GROUP,
+        (item) =>
+            day && item.day && (item.day.includes(day.toLowerCase()) || day.toLowerCase() === "sunday"),
+    );
+    
+    const characterTodayComparator = (char) => {
+        let check = false;
+        char.material["book"].map((item) => {
+            if (item.day.includes(day.toLowerCase()) || day.toLowerCase() === "sunday") {
+                check = true;
+                if (item.parent) {
+                    // item already added in the items
+                    if (!(item.parent in items)) {
+                        items[item.parent] = [
+                            {
+                                name: char.id,
+                                element: char.element.id,
+                            },
+                        ];
+                    } else {
+                        if (
+                            items[item.parent].filter(
+                                (c) => c.name === char.id,
+                            ).length === 0
+                        ) {
+                            items[item.parent].push({
+                                name: char.id,
+                                element: char.element.id,
+                            });
+                        }
+                    }
+                }
+            }
+            // } else if (day === 'Sunday') {
+            //     break
+            // }
+        });
+        return check;
+    };
+
+    let characters: Array<Character> = await compare(
+        CHARACTERS,
+        characterTodayComparator,
+    );
+
+    items["weapons"] = {}
+
+    const weaponsTodayComparator = (weapon) => {
+        let check = false;
+        let material = weapon["ascension"][0].items[0].item;
+        if (material.day.includes(day.toLowerCase()) || day.toLowerCase() === "sunday") {
+            if (!(material.id in items["weapons"])) {
+                items["weapons"][material.id] = [{
+                    id: weapon.id,
+                    rarity: weapon.rarity,
+                }]
+            } else {
+                items["weapons"][material.id].push({
+                    id: weapon.id,
+                    rarity: weapon.rarity,
+                })
+            }
+            return true;
+        }
+        return check;
+    };
+    let weapons: Array<Weapon> = await compare(
+        WEAPONS,
+        weaponsTodayComparator,
+    );
+    // console.log("daily items: ", items["weapons"]);
+
+    // items["weapons"] = weapons.map(w => {
+    //     return {
+    //         id: w.id, rarity: w.rarity
+    //     }
+    // });
+
+    var store = require('store')
+    let dailyImage = store.get('dailyImage')
+    // store.set('user', { name:'Marcus' })
+    if (!dailyImage) {
+        // let cachedDate = new Date(dailyImage.date)
+        // // let hourDifference = Math.abs(today.getTime() - cachedDate.getTime()) / 3600000;
+        // // if the daily image is no longer relevant for today
+        // if ((cachedDate.getDate() < today.getDate()) || (cachedDate.getDate() == today.getDate() && !(today.getHours() >= 0 && today.getHours() <= 4))) {
+        //     // generate new daily image for the new day
+        //     dailyImage = {
+        //         date: today,
+        //         decodedImage: await generateScreenshot(
+        //             "http://localhost:8000/views/daily.html?items=",
+        //             items),
+        //     }
+        // }
+        dailyImage = {
+            date: today,
+            decodedImage: await generateScreenshot(
+                "http://localhost:8000/views/daily.html?items=",
+                items,
+            ),
+        }
+    }
+    store.set('dailyImage',  dailyImage)
+
+    return {
+        date: today,
+        day,
+        materials,
+        characters,
+        weapons,
+        image: dailyImage.decodedImage,
+    };
+}
+
 @Resolver(Daily)
 export class DailyResolver {
     private daily: Daily;
@@ -16,136 +146,6 @@ export class DailyResolver {
 
     @Query((returns) => Daily)
     async getDaily(): Promise<Daily> {
-        // dummy data for now
-
-        const today = new Date(Date.now());
-        const items = {};
-        const days = [
-            "Sunday",
-            "Monday",
-            "Tuesday",
-            "Wednesday",
-            "Thursday",
-            "Friday",
-            "Saturday",
-        ];
-        const day = days[today.getDay()];
-        let materials: Array<ItemGroup>;
-        // make sure to test this
-        materials = await compare<ItemGroup>(
-            ITEM_GROUP,
-            (item) =>
-                day && item.day && (item.day.includes(day.toLowerCase()) || day.toLowerCase() === "sunday"),
-        );
-        
-        const characterTodayComparator = (char) => {
-            let check = false;
-            char.material["book"].map((item) => {
-                if (item.day.includes(day.toLowerCase()) || day.toLowerCase() === "sunday") {
-                    check = true;
-                    if (item.parent) {
-                        // item already added in the items
-                        if (!(item.parent in items)) {
-                            items[item.parent] = [
-                                {
-                                    name: char.id,
-                                    element: char.element.id,
-                                },
-                            ];
-                        } else {
-                            if (
-                                items[item.parent].filter(
-                                    (c) => c.name === char.id,
-                                ).length === 0
-                            ) {
-                                items[item.parent].push({
-                                    name: char.id,
-                                    element: char.element.id,
-                                });
-                            }
-                        }
-                    }
-                }
-                // } else if (day === 'Sunday') {
-                //     break
-                // }
-            });
-            return check;
-        };
-
-        let characters: Array<Character> = await compare(
-            CHARACTERS,
-            characterTodayComparator,
-        );
-
-        items["weapons"] = {}
-
-        const weaponsTodayComparator = (weapon) => {
-            let check = false;
-            let material = weapon["ascension"][0].items[0].item;
-            if (material.day.includes(day.toLowerCase()) || day.toLowerCase() === "sunday") {
-                if (!(material.id in items["weapons"])) {
-                    items["weapons"][material.id] = [{
-                        id: weapon.id,
-                        rarity: weapon.rarity,
-                    }]
-                } else {
-                    items["weapons"][material.id].push({
-                        id: weapon.id,
-                        rarity: weapon.rarity,
-                    })
-                }
-                return true;
-            }
-            return check;
-        };
-        let weapons: Array<Weapon> = await compare(
-            WEAPONS,
-            weaponsTodayComparator,
-        );
-        // console.log("daily items: ", items["weapons"]);
-
-        // items["weapons"] = weapons.map(w => {
-        //     return {
-        //         id: w.id, rarity: w.rarity
-        //     }
-        // });
-
-        var store = require('store')
-        console.log(store.get('dailyImage'))
-        let dailyImage = store.get('dailyImage')
-        // store.set('user', { name:'Marcus' })
-        if (dailyImage) {
-            let cachedDate = new Date(dailyImage.date)
-            // let hourDifference = Math.abs(today.getTime() - cachedDate.getTime()) / 3600000;
-            // if the daily image is no longer relevant for today
-            if ((cachedDate.getDate() < today.getDate()) || (cachedDate.getDate() == today.getDate() && !(today.getHours() >= 0 && today.getHours() <= 4))) {
-                // generate new daily image for the new day
-                dailyImage = {
-                    date: today,
-                    decodedImage: await generateScreenshot(
-                        "http://localhost:8000/views/daily.html?items=",
-                        items),
-                }
-            }
-        } else {
-            dailyImage = {
-                date: today,
-                decodedImage: await generateScreenshot(
-                    "http://localhost:8000/views/daily.html?items=",
-                    items,
-                ),
-            }
-        }
-        store.set('dailyImage',  dailyImage)
-
-        return {
-            date: today,
-            day,
-            materials,
-            characters,
-            weapons,
-            image: dailyImage.decodedImage,
-        };
+        return await generateDailyData()
     }
 }
